50716463a3a6e32128fc4104db315b0899ac3797
Kalle <qalle85@gmail.com>
2025-02-27 00:25:56 +0200

    rename test files

--- test.sh
+++ test.sh
@@ -1,19 +1,19 @@
 rm -f test-out/*.nes
 
-python3 png2nesdata.py test-in/doom.png 0f 15 26 30
-asm6 stillimage.asm test-out/doom.nes
+python3 png2nesdata.py test-in/doom-24x16.png 0f 15 26 30
+asm6 stillimage.asm test-out/doom-24x16.nes
 
-python3 png2nesdata.py test-in/lena.png 0f 16 26 30
-asm6 stillimage.asm test-out/lena.nes
+python3 png2nesdata.py test-in/lena-24x16.png 0f 16 26 30
+asm6 stillimage.asm test-out/lena-24x16.nes
 
-python3 png2nesdata.py test-in/market.png 0f 17 27 30
-asm6 stillimage.asm test-out/market.nes
+python3 png2nesdata.py test-in/market-24x16.png 0f 17 27 30
+asm6 stillimage.asm test-out/market-24x16.nes
 
-python3 png2nesdata.py test-in/pattern.png 0f 00 00 30
-asm6 stillimage.asm test-out/pattern.nes
+python3 png2nesdata.py test-in/pattern-24x16.png 0f 00 00 30
+asm6 stillimage.asm test-out/pattern-24x16.nes
 
-python3 png2nesdata.py test-in/sonic.png 0f 12 27 30
-asm6 stillimage.asm test-out/sonic.nes
+python3 png2nesdata.py test-in/sonic-24x16.png 0f 12 27 30
+asm6 stillimage.asm test-out/sonic-24x16.nes
 
 rm prg.bin
 rm chr.bin
