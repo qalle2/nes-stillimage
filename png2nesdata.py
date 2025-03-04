c233044e1f45046428b5b4ec6ada542b4cc4c6ee
Kalle <qalle85@gmail.com>
2025-03-04 07:52:51 +0200

    speed up, tidy up

--- png2nesdata.py
+++ png2nesdata.py
@@ -1,6 +1,6 @@
 # convert an image into NES graphics data
 
-import collections, itertools, os, sys
+import collections, itertools, os, sys, time
 try:
     from PIL import Image
 except ImportError:
@@ -33,8 +33,8 @@ DEFAULT_OUT_PALETTE = (0x0f, 0x00, 0x10, 0x30)
 PRG_OUT_FILE = "prg.bin"
 CHR_OUT_FILE = "chr.bin"
 
-# only decrease these if you can't let this program have all the sprites and
-# background tiles
+# only decrease these if you can't let this program have all the background
+# tiles and sprites
 MAX_BG_TILES_TO_USE             = MAX_BG_TILES
 MAX_SPRITES_PER_SCANLINE_TO_USE = MAX_SPRITES_PER_SCANLINE
 MAX_SPRITES_TO_USE              = MAX_SPRITES
@@ -117,7 +117,8 @@ def get_tile_distance(tile1, tile2):
     return sum(abs(a - b) for (a, b) in zip(tile1, tile2))
 
 def assign_tiles_to_sprites(imgTiles, imgWidth, imgHeight):
-    # assign as many 1*2-tile pairs as possible to sprites instead
+    # assign as many 1*2-tile pairs as possible to sprites;
+    # the rest will be background tiles
     # imgTiles:  list of image tile indexes starting from top left, with
     #            duplicates
     # imgWidth:  image width  in tiles
@@ -165,36 +166,108 @@ def assign_tiles_to_sprites(imgTiles, imgWidth, imgHeight):
     return (bgTiles, spriteData)
 
 def get_tile_to_replace(
-    origTileCnt, tileDistances, tileCnts, distinctTilesLeft
+    origTileCnt, tileDistances, tileCnts, distinctTilesLeft, minPossibleError=1
 ):
     # which distinct tile to eliminate with the smallest error possible?
     #   origTileCnt:       original number of distinct tiles
-    #   tileDistances:     a look-up table for distances between tiles;
+    #   tileDistances:     a table of distances between tiles;
     #                      index: (tile_index1 * origTileCnt + tile_index2)
     #   tileCnts:          {tile_index: count_in_image, ...}
     #   distinctTilesLeft: set of indexes to original distinct tiles
+    #   minPossibleError:  an int; stop when the total error is this small,
+    #                      for speed
     #   return:            tile to replace: (from_index, to_index)
 
+    # find a tile pair that minimises
+    # (distance_between_tiles * source_tile_count);
+    # it seems it would not help at all to prefer target tiles that are common
+
     minTotalDiff = -1
-    for fromInd in distinctTilesLeft:
-        if fromInd != BLANK_TILE_INDEX:
+
+    for srcInd in distinctTilesLeft:
+        if srcInd != BLANK_TILE_INDEX:
             # find the closest match for this tile
             minDiff = -1
-            for toInd in distinctTilesLeft:
-                if toInd != fromInd:
-                    dist = tileDistances[fromInd*origTileCnt+toInd]
-                    if minDiff == -1 or dist < minDiff:
-                        minDiff   = dist
-                        bestToInd = toInd
+            for dstInd in distinctTilesLeft:
+                if dstInd != srcInd:
+                    diff = tileDistances[srcInd*origTileCnt+dstInd]
+                    if minDiff == -1 or diff < minDiff:
+                        minDiff    = diff
+                        bestDstInd = dstInd
             # remember this tile and its replacement if they result in the
             # smallest total error so far
-            totalDiff = minDiff * tileCnts[fromInd]
+            totalDiff = minDiff * tileCnts[srcInd]
             if minTotalDiff == -1 or totalDiff < minTotalDiff:
-                minTotalDiff     = totalDiff
-                bestFromInd      = fromInd
-                overallBestToInd = bestToInd
+                minTotalDiff      = totalDiff
+                bestSrcInd        = srcInd
+                overallBestDstInd = bestDstInd
+                if minTotalDiff == minPossibleError:
+                    break
 
-    return (bestFromInd, overallBestToInd)
+    return (bestSrcInd, overallBestDstInd)
+
+def eliminate_tiles(origDistinctImgTiles, origImgTileIndexes, imgWidth):
+    # if there are too many distinct tiles in the image, eliminate them
+    # origDistinctImgTiles: pixels of each originally distinct tile;
+    #                       does not change
+    # origImgTileIndexes:   which tile index was originally in each tile
+    #                       position; does not change
+    # imgWidth:             image width in tiles
+    # return:               new tile indexes in each tile position
+
+    imgHeight = len(origImgTileIndexes) // imgWidth  # image height in tiles
+
+    # a table of distances between any two tiles; does not change
+    origTileDistances = []
+    for tile1 in origDistinctImgTiles:
+        for tile2 in origDistinctImgTiles:
+            origTileDistances.append(get_tile_distance(tile1, tile2))
+
+    # which tile index is in each tile position;
+    # updated whenever a tile is eliminated
+    imgTileIndexes = origImgTileIndexes.copy()
+
+    # indexes to origDistinctImgTiles;
+    # tells us which tiles haven't been eliminated yet
+    distinctImgTilesLeft = set(range(len(origDistinctImgTiles)))
+
+    # the smallest possible error on each round of tile elimination;
+    # it never decreases, so it lets us stop searching early for a ~30% speedup
+    # and no loss of quality
+    minPossibleError = 1
+
+    while True:
+        # assign remaining image tiles to sprites and background
+        (bgTileIndexes, spriteData) = assign_tiles_to_sprites(
+            imgTileIndexes, imgWidth, imgHeight
+        )
+
+        # get number of distinct background tiles
+        distinctBgTileCnt = len(set(bgTileIndexes) | set((BLANK_TILE_INDEX,)))
+
+        if distinctBgTileCnt <= MAX_BG_TILES_TO_USE:
+            break
+        else:
+            # replace a tile with one that will cause the smallest total error
+            (from_, to_) = get_tile_to_replace(  # TODO: inline?
+                len(origDistinctImgTiles),
+                origTileDistances,
+                collections.Counter(imgTileIndexes),
+                distinctImgTilesLeft,
+                minPossibleError
+            )
+            minPossibleError = max(
+                minPossibleError,
+                origTileDistances[from_*len(origDistinctImgTiles)+to_]
+                * imgTileIndexes.count(from_)
+            )
+
+            imgTileIndexes = [
+                (to_ if i == from_ else i) for i in imgTileIndexes
+            ]
+            distinctImgTilesLeft.remove(from_)
+
+    return imgTileIndexes
 
 def encode_at_data(atData):
     # encode attribute table data
@@ -206,12 +279,12 @@ def encode_at_data(atData):
 
     for y in range(8):
         for x in range(8):
-            si = (y * 16 + x) * 2  # source index
+            srcInd = (y * 16 + x) * 2
             yield (
-                   atData[si     ]
-                | (atData[si+   1] << 2)
-                | (atData[si+16  ] << 4)
-                | (atData[si+16+1] << 6)
+                   atData[srcInd     ]
+                | (atData[srcInd+   1] << 2)
+                | (atData[srcInd+16  ] << 4)
+                | (atData[srcInd+16+1] << 6)
             )
 
 def get_prg_data(ntData, spriteData, outputPal, imgWidth, imgHeight):
@@ -267,6 +340,7 @@ def encode_tile(tile):
 # --- main --------------------------------------------------------------------
 
 def main():
+    startTime = time.time()
     (inputFile, outputPal) = parse_arguments()
 
     # read tiles
@@ -280,96 +354,70 @@ def main():
     except OSError:
         sys.exit("Error reading input file.")
 
-    # note: some distinct tiles will be eliminated (replaced with similar
-    # tiles) if necessary
-
-    # what each distinct tile looks like;
+    # pixels of each originally distinct tile;
     # does not change during elimination of tiles
     origDistinctImgTiles = sorted(set(imgTiles) | set((BLANK_TILE,)))
-
-    # indexes to origDistinctImgTiles;
-    # tells us which tiles haven't been eliminated yet
-    distinctImgTilesLeft = set(range(len(origDistinctImgTiles)))
-
-    # a table of distances between any two tiles;
-    # does not change during elimination of tiles
-    origTileDistances = []
-    for tile1 in origDistinctImgTiles:
-        for tile2 in origDistinctImgTiles:
-            origTileDistances.append(get_tile_distance(tile1, tile2))
 
     # which tile index was originally in each tile position;
     # does not change during elimination of tiles;
     # used for calculating total error
     origImgTileIndexes = [origDistinctImgTiles.index(t) for t in imgTiles]
 
-    # which tile index is in each tile position;
-    # updated whenever a tile is eliminated
-    imgTileIndexes = origImgTileIndexes.copy()
-
     print("Image has {} distinct tiles. Eliminating tiles if needed.".format(
         len(origDistinctImgTiles)
     ))
-    eliminatedTileCnt = 0
 
-    while True:
-        # assign as many image tiles as possible to sprites; the rest will be
-        # background tiles
-        (bgTileIndexes, spriteData) = assign_tiles_to_sprites(
-            imgTileIndexes, imgWidth, imgHeight
-        )
+    # eliminate distinct tiles if necessary
+    imgTileIndexes = eliminate_tiles(
+        origDistinctImgTiles, origImgTileIndexes, imgWidth
+    )
 
-        # get number of distinct background tiles
-        distinctBgTileCnt = len(set(bgTileIndexes) | set((BLANK_TILE_INDEX,)))
+    # reassign remaining image tiles to sprites and background
+    (bgTileIndexes, spriteData) = assign_tiles_to_sprites(
+        imgTileIndexes, imgWidth, imgHeight
+    )
 
-        if distinctBgTileCnt <= MAX_BG_TILES_TO_USE:
-            break
-        else:
-            # replace a tile with one that will cause the smallest total error
-            (from_, to_) = get_tile_to_replace(
-                len(origDistinctImgTiles), origTileDistances,
-                collections.Counter(imgTileIndexes), distinctImgTilesLeft
-            )
-            imgTileIndexes = [
-                (to_ if i == from_ else i) for i in imgTileIndexes
-            ]
-            distinctImgTilesLeft.remove(from_)
-            eliminatedTileCnt += 1
+    distinctBgTileIndexes = set(bgTileIndexes) | set((BLANK_TILE_INDEX,))
+    if (
+           len(distinctBgTileIndexes) > MAX_BG_TILES_TO_USE
+        or len(spriteData)            > MAX_SPRITES_TO_USE
+    ):
+        sys.exit("Crosscheck failed (this should never happen).")
 
     totalError = sum(
-        origTileDistances[t1*len(origDistinctImgTiles)+t2]
+        get_tile_distance(origDistinctImgTiles[t1], origDistinctImgTiles[t2])
         for (t1, t2) in zip(origImgTileIndexes, imgTileIndexes)
     )
-    # ratio=1 if the entire image changed between darkest and brightest
+    # the error ratio will be 1 if the entire image changed between the darkest
+    # and the lightest colour
     totalErrorRatio = (
         totalError / (imgWidth * imgHeight * TILE_WIDTH * TILE_HEIGHT * 3)
     )
 
     print(
-        "{} tiles eliminated for a total error of {} units ({:.1f}%).".format(
-            eliminatedTileCnt, totalError, totalErrorRatio * 100
+        "Total error from elimination of tiles: {} units ({:.1f}%).".format(
+            totalError, totalErrorRatio * 100
         )
     )
     print(
         "Image has {} distinct tiles ({} sprites and {} distinct background "
         "tiles).".format(
-            len(distinctImgTilesLeft), len(spriteData), distinctBgTileCnt
+            len(set(imgTileIndexes) | set((BLANK_TILE_INDEX,))),
+            len(spriteData),
+            len(distinctBgTileIndexes)
         )
     )
 
-    del distinctImgTilesLeft
-
     # get pixels of distinct background tiles
     distinctBgTiles = sorted(
-        origDistinctImgTiles[i]
-        for i in (set(bgTileIndexes) | set((BLANK_TILE_INDEX,)))
+        origDistinctImgTiles[i] for i in distinctBgTileIndexes
     )
     # convert background tile indexes from image-wide to background-wide
     bgTileIndexes = [
         distinctBgTiles.index(origDistinctImgTiles[i]) for i in bgTileIndexes
     ]
 
-    # get data of distinct pairs of sprite tiles
+    # get pixels of distinct pairs of sprite tiles
     distinctSprTileIndPairs = set((t1, t2) for (x, y, t1, t2) in spriteData)
     distinctSprTilePairs = sorted(
         (origDistinctImgTiles[t1], origDistinctImgTiles[t2])
@@ -387,6 +435,8 @@ def main():
     # sort sprites by Y and X
     spriteData.sort(key=lambda s: (s[1], s[0]))
 
+    # TODO: deduplicate sprite tile pair data by flipping?
+
     # write PRG data
     try:
         with open(PRG_OUT_FILE, "wb") as handle:
@@ -396,7 +446,6 @@ def main():
             )))
     except OSError:
         sys.exit(f"Error writing {PRG_OUT_FILE}")
-    print(f"Wrote {PRG_OUT_FILE}")
 
     # combine and pad tile data to (MAX_BG_TILES + MAX_SPRITES * 2) tiles;
     # a tile is a tuple of (TILE_WIDTH * TILE_HEIGHT) 2-bit ints
@@ -420,6 +469,9 @@ def main():
             )))
     except OSError:
         sys.exit(f"Error writing {CHR_OUT_FILE}")
-    print(f"Wrote {CHR_OUT_FILE}")
+
+    print("Wrote {} and {} in {:.1f} seconds.".format(
+        PRG_OUT_FILE, CHR_OUT_FILE, time.time() - startTime
+    ))
 
 main()
